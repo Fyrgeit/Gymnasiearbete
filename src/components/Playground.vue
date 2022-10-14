2380f203e3c45a366c4d935e1e3611f489b71a98
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-10-14 13:45:01 +0200

    yes

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -9,7 +9,7 @@
 
 export default {
     name: "Playground",
-    
+
     mounted: function () {
         this.$nextTick(function () {
             console.log("h");
@@ -24,36 +24,59 @@ function draw() {
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
 
-        ctx.lineWidth = 5;
-        
+        ctx.lineWidth = 3;
+
         ctx.beginPath();
         ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
         ctx.stroke();
-        
-        eye(ctx, 55, 65, 15);
-        eye(ctx, 95, 65, 15);
 
-        pupil(ctx, 55, 65, 10);
-        pupil(ctx, 95, 65, 10);
-        
+        eye(1, ctx, 55, 65, 10);
+        pupil(ctx, 55, 65, 0);
+
         ctx.beginPath();
         ctx.arc(75, 55, 50, 1, Math.PI - 1, false);
         ctx.stroke();
     }
 };
 
-function eye(ctx, x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y-5, r, Math.PI, 0, false);
-    ctx.lineTo(x+r, y+r/2);
-    ctx.arc(x, y+5, r, 0, Math.PI, false);
-    ctx.lineTo(x-r, y-r/2);
-    ctx.stroke();
+function eye(type, ctx, x, y, r) {
+    switch (type) {
+        case 0:
+            ctx.beginPath();
+            ctx.arc(x, y, r, 0, Math.PI * 2);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(150 - x, y, r, 0, Math.PI * 2);
+            ctx.stroke();
+            break;
+        case 1:
+            ctx.beginPath();
+            ctx.arc(x, y - 5, r, Math.PI, 0, false);
+            ctx.lineTo(x + r, y + r / 2);
+            ctx.arc(x, y + 5, r, 0, Math.PI, false);
+            ctx.lineTo(x - r, y - r / 2);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(150 - x, y - 5, r, Math.PI, 0, false);
+            ctx.lineTo(150 - x + r, y + r / 2);
+            ctx.arc(150 - x, y + 5, r, 0, Math.PI, false);
+            ctx.lineTo(150 - x - r, y - r / 2);
+            ctx.stroke();
+            break;
+        default:
+            break;
+    }
 };
 
 function pupil(ctx, x, y, r) {
     ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI*2);
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(150 - x, y, r, 0, Math.PI * 2);
     ctx.fill();
 };
 
